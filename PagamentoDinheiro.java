e16e6a56776a4624f4fbebedfe29ac45ee395096
Moisés Fênix <moisesfenix14@gmail.com>
2026-06-26 19:22:20 -0300

    Update PagamentoDinheiro.java

--- PagamentoDinheiro.java
+++ PagamentoDinheiro.java
@@ -1,12 +1,12 @@
 public class PagamentoDinheiro extends Pagamento {
 
-    private static final double DESCONTO = 0.05; // 5% de desconto
+    private static final double DESCONTO = 0.05; 
 
     public PagamentoDinheiro(double valor, String dataPagamento) {
         super(valor, dataPagamento);
     }
 
-    // SOBRESCRITA
+  
     @Override
     public double calcularValorFinal() {
         return valor - (valor * DESCONTO);
